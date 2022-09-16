c4059082ea5aca3d6312d1e47022ffc074f263c0
docktermj <michael@dockter.com>
2022-09-16 17:36:22 -0400

    #22 Working example

--- init-postgresql.py
+++ init-postgresql.py
@@ -34,7 +34,7 @@ from senzing import G2Config, G2ConfigMgr, G2ModuleException
 # Metadata
 
 __all__ = []
-__version__ = "1.1.0"  # See https://www.python.org/dev/peps/pep-0396/
+__version__ = "1.1.1"  # See https://www.python.org/dev/peps/pep-0396/
 __date__ = '2022-08-04'
 __updated__ = '2022-09-16'
 
