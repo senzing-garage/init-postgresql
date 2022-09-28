ad46dfe1966f0013154ba03249e821c4ed9a1d03
docktermj <michael@dockter.com>
2022-09-28 11:33:10 -0400

    #27 Prepare for versioning

--- init-postgresql.py
+++ init-postgresql.py
@@ -34,9 +34,9 @@ from senzing import G2Config, G2ConfigMgr, G2ModuleException
 # Metadata
 
 __all__ = []
-__version__ = "1.1.1"  # See https://www.python.org/dev/peps/pep-0396/
+__version__ = "1.1.2"  # See https://www.python.org/dev/peps/pep-0396/
 __date__ = '2022-08-04'
-__updated__ = '2022-09-16'
+__updated__ = '2022-09-28'
 
 # See https://github.com/Senzing/knowledge-base/blob/main/lists/senzing-product-ids.md
 
